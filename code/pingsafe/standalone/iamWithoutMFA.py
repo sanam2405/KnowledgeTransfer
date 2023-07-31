814a10a9e97dedf17d4625ec77579fb099932d70
Manas Pratim Biwas <manaspb2405@gmail.com>
2023-07-31 13:19:50 +0530

    Moved the standalone scripts to new dir.

---
+++ code/pingsafe/standalone/iamWithoutMFA.py
@@ -0,0 +1,35 @@
+'''
+
+List all the IAM Users that do not have 
+Multi Factor Authentication (MFA) enabled
+
+'''
+import boto3
+
+def get_iam_users():
+    iam = boto3.client('iam')
+    response = iam.list_users()
+
+    users = response['Users']
+    while response['IsTruncated']:
+        response = iam.list_users(Marker=response['Marker'])
+        users.extend(response['Users'])
+
+    return users
+
+def list_users_without_mfa(users):
+    print("IAM Users without MFA Active:")
+
+    for user in users:
+        username = user['UserName']
+        mfa_devices = boto3.client('iam').list_mfa_devices(UserName=username)['MFADevices']
+        if not mfa_devices:
+            print(f"Username: {username}")
+            print("------")
+
+def main():
+    iam_users = get_iam_users()
+    list_users_without_mfa(iam_users)
+
+if __name__ == '__main__':
+    main()
---
+++ code/pingsafe/standalone/kms.py
@@ -0,0 +1,37 @@
+'''
+
+List all the Keys where
+KMS Key rotation is not enabled
+
+'''
+import boto3
+
+def get_kms_client():
+    return boto3.client('kms')
+
+def get_non_rotated_keys():
+    kms_client = get_kms_client()
+    response = kms_client.list_keys()
+
+    non_rotated_keys = []
+    for key_id in response['Keys']:
+        key_metadata = kms_client.describe_key(KeyId=key_id['KeyId'])
+        if 'KeyMetadata' in key_metadata:
+            key_rotation_enabled = key_metadata['KeyMetadata'].get('KeyRotationEnabled', False)
+            if not key_rotation_enabled:
+                non_rotated_keys.append(key_id['KeyId'])
+
+    return non_rotated_keys
+
+def main():
+    non_rotated_keys = get_non_rotated_keys()
+
+    if non_rotated_keys:
+        print("KMS keys without key rotation enabled:")
+        for key_id in non_rotated_keys:
+            print(key_id)
+    else:
+        print("No KMS keys found without key rotation enabled.")
+
+if __name__ == '__main__':
+    main()
